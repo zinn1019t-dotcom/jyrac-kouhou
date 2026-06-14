47c946b93b488e7caca90a6837efd8714a7c956e
Takaesu._.p <zinn.1019t@gmail.com>
2026-06-14 15:37:41 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// JYRAC 広報局ポータル 外部設定ファイル
+const JYRAC_CONFIG = {
+    // 議事録（Googleドキュメント）のURL
+    minutesUrl: "https://docs.google.com/document/d/15GyUS29yVTQpGufv3dUUEkIeD69bLiSnTCweasdxFvQ/edit",
+    
+    // 日程調整（調整さん）のURL
+    scheduleUrl: "https://chouseisan.com/s?h=7ed52b12e8f14deab71405f2dea3c99a",
+    
+    // GAS (Google Apps Script) 通知用URL
+    gasWebhookUrl: "https://script.google.com/macros/s/AKfycbyv49XwEzcl7ARaxdasx6Uo667B_KQiwgJYbV79wL_IsNuLuvBV-cEZ_8pfvHr__i3R/exec"
+};
